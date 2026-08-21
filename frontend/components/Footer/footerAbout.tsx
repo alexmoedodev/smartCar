482e72f46ab49ee8e6d687e074e9c114f8cb9db1
AlexMoedodev <alexmoedodev@gmail.com>
2026-08-21 14:54:24 -0300

    feat:Centraliza as informações utilizadas para renderizar os links

---
+++ frontend/components/Footer/footerAbout.tsx
@@ -0,0 +1,54 @@
+/**
+ * Configuração dos itens de navegação do Footer - Sobre-nós.
+ * 
+ * Centraliza as informação utilizadas para renderizar os links de navegação facilitando a adição
+ * e remoção em um unico arquivo.
+ * 
+ * ATENÇÃO:
+ * As alterções realizadas nesse arquivo são refletidas automaticamente nos 
+ * componentes qye utilizam.
+ * 
+ * `label` - Texto exibido no link de navegação.
+ * `title` - Texto exibido como tooltip ao passar o cursor sobre o link.
+ * `href` -  URL destino.
+ * `icon` - Ícone correspondente ao item de navegação.
+ */
+
+import iconDefault from "@/constants/icons"
+/**
+ * Tamanho padrão do icone renderização.
+ */
+const iconSizeDefault: number = 18
+
+const footerAbout = [
+    {
+        label: "Horários de Funcionamento",
+        title: "Horários de funcionamento.",
+        href: "/horarios-funcionamento-agencia",
+        icon: iconDefault.others.timer,
+        iconSize: iconSizeDefault
+    },
+    {
+        label: "Contatos",
+        title: "Número para contato.",
+        href: "/contato-agencia",
+        icon: iconDefault.contact.phone,
+        iconSize: iconSizeDefault
+    },
+    {
+        label: "Localização",
+        title: "Nossa localização.",
+        href: "/localizacao-agencia",
+        icon: iconDefault.others.map,
+        iconSize: iconSizeDefault
+    },
+    {
+        label: "Sobre-nós",
+        title: "Saiba mais sobre-nós.",
+        href: "/sobre-nos-agencia",
+        icon: iconDefault.others.about,
+        iconSize: iconSizeDefault
+    },
+]
+
+export default footerAbout
