6af282a69f70b9eddf50ff3b0c0182e030465aa1
Alex <alexmoedodev@gmail.com>
2026-08-26 10:26:52 -0300

    feat(Input) creacao do input reutilizavel r documentacao do arquivo

--- frontend/components/ux/Input.tsx
+++ frontend/components/ux/Input.tsx
@@ -1,36 +1,35 @@
-import React from 'react'
+import React from "react";
 
-interface Props {
-    label?: string
-    htmlFor?: string
-    inputSize: "xs" | "sm" | "md" | "lg" | "fit" | "full"
-}  
+type Props = {
+  label?: string;
+  htmlFor?: string;
+  inputSize?: "xs" | "sm" | "md" | "lg" | "fit" | "full";
+  
+} & React.InputHTMLAttributes<HTMLInputElement>;
 
-const Input = ({ label, htmlFor, inputSize = "sm", ...props }: Props) => {
+const Input = ({ label, htmlFor, inputSize = "fit", ...props }: Props) => {
+  const variantSize = {
+    xs: "w-xs",
+    sm: "w-sm",
+    md: "w-md",
+    lg: "w-full sm:w-lg",
+    fit: "w-fit",
+    full:"w-full"
+  }; 
 
-    const variantSize = {
-        xs: "w-xs",
-        sm: "w-sm",
-        md: "w-md",
-        lg: "w-lg",
-        fit: "w-fit",
-        full: "w-full"
-    }
+  return (
+    <div className={`flex flex-col gap-1 ${inputSize === "full" ? "w-full" : "w-auto"}`}>
+      {label && (
+        <label className="text-sm text-black" htmlFor={htmlFor}>
+          {label}
+        </label>
+      )}
+      <input
+        className={`${variantSize[inputSize]}  border-0 rounded-lg bg-gray-100 px-2 sm:py-2 sm:px-3`}
+        {...props}
+      />
+    </div>
+  );
+};
 
-
-    return (
-        <div>
-            {label && (
-                <label htmlFor={htmlFor}>
-                    {label}
-                </label>
-            )}
-            <input
-                className={`${variantSize[inputSize]} border-0 rounded-lg bg-gray-100 py-1 px-2`}
-                {...props}
-            />
-        </div>
-    )
-}
-
-export default Input
+export default Input;
