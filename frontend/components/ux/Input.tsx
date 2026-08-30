a2495096a5a82dc8b6a1f4dac3b1a670a3cb95eb
Alex <alexmoedodev@gmail.com>
2026-08-29 22:00:01 -0300

    chore: update frontend/components/ux/Input.tsx

--- frontend/components/ux/Input.tsx
+++ frontend/components/ux/Input.tsx
@@ -7,7 +7,24 @@ type Props = {
   
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
+/**
+ * Componente responsável por manter um layout padrão do `input` da aplicação.
+ * 
+ * ATENÇÃO:
+ * Alteração desse arquivo pode refletir em todo os componentes que utilizam.
+ * 
+ * @param label - Responsável por exibir o texto acima do input.
+ * Propriedade é opcional, ao não utilizar a propriedade o texto não será exibido.
+ * @param htmlFor - Responsável por acossiar a label ao ID do input. 
+ * @param selectinput
+ *
+ */
+ 
 const Input = ({ label, htmlFor, inputSize = "fit", ...props }: Props) => {
+
+  /**
+   * Responsável por definir qual tamanho do input a ser renderizado.
+   */
   const variantSize = {
     xs: "w-xs",
     sm: "w-sm",
@@ -18,14 +35,14 @@ const Input = ({ label, htmlFor, inputSize = "fit", ...props }: Props) => {
   }; 
 
   return (
-    <div className={`flex flex-col gap-1 ${inputSize === "full" ? "w-full" : "w-auto"}`}>
+    <div className={`flex flex-col gap-1 ${inputSize === "full" ? "w-full" : "sm:w-auto"}`}>
       {label && (
         <label className="text-sm text-black" htmlFor={htmlFor}>
           {label}
         </label>
       )}
       <input
-        className={`${variantSize[inputSize]}  border-0 rounded-lg bg-gray-100 px-2 sm:py-2 sm:px-3`}
+        className={`${variantSize[inputSize]}  border-0 rounded-lg bg-gray-100 py-2 px-3`}
         {...props}
       />
     </div>
