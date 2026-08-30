de2b9ea7f14379e2f5afc2e441b34237405f83ca
Alex <alexmoedodev@gmail.com>
2026-08-29 22:00:09 -0300

    chore: add frontend/constants/messages/tittleMessage.ts

---
+++ frontend/constants/messages/tittleMessage.ts
@@ -0,0 +1,20 @@
+
+/**
+ * Responsávelrenderização e por manter um padrão de 
+ * das mesagens nos title (tooplit).
+ * 
+ * ATENÇÃO:
+ * Alteração nesse arquivo reflete em todos os componentes que utilizam.
+ * 
+ * - search: Mensagens de title para campos que realizam filtros.
+ */
+export const titlteMessage = {
+    
+    search: {
+        nameVehicle: `Digite o nome do veiculo que deseja realizar a busca.`,
+        markVehicle: `Selecione a marca do veiculo que deseja realizar a busca.`,
+        yearFabricationVehicle: "Selecione o ano de fabicação do veiculo que deseja realizar a busca.",
+    },
+
+
+}
