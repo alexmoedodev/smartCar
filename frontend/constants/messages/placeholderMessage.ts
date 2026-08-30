6401f798e9259cfdd62baf425fcaffa96a77c5fa
Alex <alexmoedodev@gmail.com>
2026-08-29 22:00:08 -0300

    chore: add frontend/constants/messages/placeholderMessage.ts

---
+++ frontend/constants/messages/placeholderMessage.ts
@@ -0,0 +1,14 @@
+/**
+ * Responsávelrenderização e por manter um padrão de 
+ * das mesagens nos placehoder.
+ * 
+ * ATENÇÃO:
+ * Alteração nesse arquivo reflete em todos os componentes que utilizam.
+ * 
+ * - search: Mensagens de placeholder para campos que realizam filtros.
+ */
+export const placeholderMessage = {
+    search: {
+        nameVehicle: `Digite o nome do veiculo...`,
+    }
+}
