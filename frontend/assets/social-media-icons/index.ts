5cd27db588c6e9587152595dff4ca9430a3a08a7
Alex <alexmoedodev@gmail.com>
2026-08-29 22:00:02 -0300

    chore: add frontend/assets/social-media-icons/index.ts

---
+++ frontend/assets/social-media-icons/index.ts
@@ -0,0 +1,18 @@
+/**
+ * SVG - Icones Redes sociais 
+ */
+import Instagram from "@/assets/social-media-icons/instagram.svg"
+import Facebook from "@/assets/social-media-icons/facebook.svg"
+import X_SocialMedia from "@/assets/social-media-icons/x.svg"
+import TikTok from "@/assets/social-media-icons/tiktok.svg"
+import WhatsApp from "@/assets/social-media-icons/whatsapp.svg"
+import YouTube from "@/assets/social-media-icons/youtube.svg"
+
+export{
+Instagram,
+Facebook,
+X_SocialMedia,
+TikTok,
+WhatsApp,
+YouTube
+}
