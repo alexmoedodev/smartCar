b40f233756afa2a5cbde9ef9e00ffbbe582707b9
Alex <alexmoedodev@gmail.com>
2026-08-29 21:59:59 -0300

    chore: update frontend/app/page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import  NavBar  from '@/components/NavBar/NavBar'
-import  Hero  from '@/components/Hero/Hero'
+import NavBar from '@/components/NavBar/NavBar'
+import Hero from '@/components/Hero/Hero'
 import Footer from '@/components/Footer/Footer'
 import FilterCar from '@/components/FilterCatalogue/FilterCar'
+import ContainerCars from '@/components/FilterCatalogue/ContainerCars'
+import CarroselVehicleBrands from '@/components/Carrocel/CarroselVehicleBrands'
 
 function Home() {
   return (
     <>
       <NavBar />
       <Hero />
-      <FilterCar/>
+      <CarroselVehicleBrands />
+      <FilterCar />
+      <ContainerCars />
       <Footer />
     </>
   )
