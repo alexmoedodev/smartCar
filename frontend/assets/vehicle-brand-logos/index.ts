a82c62700247fe2972c46f50b2d8bb8445cab5da
Alex <alexmoedodev@gmail.com>
2026-08-29 22:00:05 -0300

    chore: add frontend/assets/vehicle-brand-logos/index.ts

---
+++ frontend/assets/vehicle-brand-logos/index.ts
@@ -0,0 +1,49 @@
+import IconAudi from "./audi.svg";
+import IconBMW from "./bmw.svg";
+import IconChevrolet from "./chevrolet.svg";
+import IconChrysler from "./chrysler.svg";
+import IconCitroen from "./citroen.svg";
+import IconFiat from "./fiat.svg";
+import IconFord from "./ford.svg";
+import IconGeneralMotors from "./generalmotors.svg";
+import IconHonda from "./honda.svg";
+import IconHyundai from "./hyundai.svg";
+import IconJeep from "./jeep.svg";
+import IconMitsubishi from "./mitsubishi.svg";
+import IconNissan from "./nissan.svg";
+import IconPeugeot from "./peugeot.svg";
+import IconPorsche from "./porsche.svg";
+import IconRam from "./ram.svg";
+import IconRenault from "./renault.svg";
+import IconSubaru from "./subaru.svg";
+import IconSuzuki from "./suzuki.svg";
+import IconTesla from "./tesla.svg";
+import IconToyota from "./toyota.svg";
+import IconVolkswagen from "./volkswagen.svg";
+import IconVolvo from "./volvo.svg";
+
+export {
+    IconAudi,
+    IconBMW,
+    IconChevrolet,
+    IconChrysler,
+    IconCitroen,
+    IconFiat,
+    IconFord,
+    IconGeneralMotors,
+    IconHonda,
+    IconHyundai,
+    IconJeep,
+    IconMitsubishi,
+    IconNissan,
+    IconPeugeot,
+    IconPorsche,
+    IconRam,
+    IconRenault,
+    IconSubaru,
+    IconSuzuki,
+    IconTesla,
+    IconToyota,
+    IconVolkswagen,
+    IconVolvo,
+};
