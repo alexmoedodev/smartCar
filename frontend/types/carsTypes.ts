f77869288224d59e28f7e53a7b3088f57da665f1
Alex <alexmoedodev@gmail.com>
2026-08-29 22:00:12 -0300

    chore: add frontend/types/carsTypes.ts

---
+++ frontend/types/carsTypes.ts
@@ -0,0 +1,13 @@
+export type CarsTypes = {
+    id: number
+    model: string
+    mark: string
+    price: number
+    km: string
+    fuel: string
+    transmission: string
+    year: number
+    occupants: number
+    description: string
+    image: string
+}
