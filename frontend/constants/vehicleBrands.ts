89c2e68f33bfe1620fe029866d8c500d1fe2bcef
Alex <alexmoedodev@gmail.com>
2026-08-29 22:00:09 -0300

    chore: add frontend/constants/vehicleBrands.ts

---
+++ frontend/constants/vehicleBrands.ts
@@ -0,0 +1,131 @@
+/**
+ * Função responsável por renderizar a listagem de  marcas dos veiculos
+ * 
+ * ATENÇÃO:
+ * Alterção nesse arquivo pode refletir em todos os componentes que utilizam.
+ * 
+ * - label : Renderiza o Nome da Marca.
+ * - value : valor utlizado no option do select.
+ */
+export const vehicleBrands = [
+    {
+        label: "Fiat",
+        value: "fiat",
+    },
+    {
+        label: "Volkswagen",
+        value: "volkswagen",
+    },
+    {
+        label: "Chevrolet",
+        value: "chevrolet",
+    },
+    {
+        label: "Ford",
+        value: "ford",
+    },
+    {
+        label: "Toyota",
+        value: "toyota",
+    },
+    {
+        label: "Honda",
+        value: "honda",
+    },
+    {
+        label: "Hyundai",
+        value: "hyundai",
+    },
+    {
+        label: "Renault",
+        value: "renault",
+    },
+    {
+        label: "Nissan",
+        value: "nissan",
+    },
+    {
+        label: "Jeep",
+        value: "jeep",
+    },
+    {
+        label: "CAOA Chery",
+        value: "caoa-chery",
+    },
+    {
+        label: "BYD",
+        value: "byd",
+    },
+    {
+        label: "GWM",
+        value: "gwm",
+    },
+    {
+        label: "Geely",
+        value: "geely",
+    },
+    {
+        label: "Omoda Jaecoo",
+        value: "omoda-jaecoo",
+    },
+    {
+        label: "GAC",
+        value: "gac",
+    },
+    {
+        label: "Mitsubishi",
+        value: "mitsubishi",
+    },
+    {
+        label: "Peugeot",
+        value: "peugeot",
+    },
+    {
+        label: "Citroën",
+        value: "citroen",
+    },
+    {
+        label: "RAM",
+        value: "ram",
+    },
+    {
+        label: "Kia",
+        value: "kia",
+    },
+    {
+        label: "JAC",
+        value: "jac",
+    },
+    {
+        label: "BMW",
+        value: "bmw",
+    },
+    {
+        label: "Mercedes-Benz",
+        value: "mercedes-benz",
+    },
+    {
+        label: "Audi",
+        value: "audi",
+    },
+    {
+        label: "Volvo",
+        value: "volvo",
+    },
+    {
+        label: "Land Rover",
+        value: "land-rover",
+    },
+    {
+        label: "Porsche",
+        value: "porsche",
+    },
+    {
+        label: "Suzuki",
+        value: "suzuki",
+    },
+    {
+        label: "Subaru",
+        value: "subaru",
+    },
+]
