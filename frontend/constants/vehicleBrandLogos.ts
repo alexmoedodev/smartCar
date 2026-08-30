d9a40c04c5480183fbbaa63db2c81cd14a25ed11
Alex <alexmoedodev@gmail.com>
2026-08-29 22:00:09 -0300

    chore: add frontend/constants/vehicleBrandLogos.ts

---
+++ frontend/constants/vehicleBrandLogos.ts
@@ -0,0 +1,67 @@
+import {
+    IconAudi,
+    IconBMW,
+    IconChevrolet,
+    IconChrysler,
+    IconCitroen,
+    IconFiat,
+    IconFord,
+    IconGeneralMotors,
+    IconHonda,
+    IconHyundai,
+    IconJeep,
+    IconMitsubishi,
+    IconNissan,
+    IconPeugeot,
+    IconPorsche,
+    IconRam,
+    IconRenault,
+    IconSubaru,
+    IconSuzuki,
+    IconTesla,
+    IconToyota,
+    IconVolkswagen,
+    IconVolvo,
+} from "@/assets/vehicle-brand-logos";
+
+
+/**
+ * Centraliza os ícones utilizados para icones das marcas dos veiculos.
+ *
+ * O objetivo deste arquivo é manter os ícones organizados em um único local,
+ * facilitando a manutenção, substituição e padronização da aparência dos ícones.
+ *
+ * Os ícones são organizados de acordo com sua finalidade dentro da aplicação,
+ * evitando que diferentes partes do projeto precisem definir individualmente
+ * propriedades que devem seguir um padrão.
+ *
+ * ATENÇÃO:
+ * Alterações realizadas neste arquivo podem afetar todos os componentes que
+ * utilizam os ícones aqui definidos.
+ *
+ */
+export const vehicleBrandLogos = [
+    { name: "Audi", icon: IconAudi },
+    { name: "BMW", icon: IconBMW },
+    { name: "Chevrolet", icon: IconChevrolet },
+    { name: "Chrysler", icon: IconChrysler },
+    { name: "Citroën", icon: IconCitroen },
+    { name: "Fiat", icon: IconFiat },
+    { name: "Ford", icon: IconFord },
+    { name: "General Motors", icon: IconGeneralMotors },
+    { name: "Honda", icon: IconHonda },
+    { name: "Hyundai", icon: IconHyundai },
+    { name: "Jeep", icon: IconJeep },
+    { name: "Mitsubishi", icon: IconMitsubishi },
+    { name: "Nissan", icon: IconNissan },
+    { name: "Peugeot", icon: IconPeugeot },
+    { name: "Porsche", icon: IconPorsche },
+    { name: "RAM", icon: IconRam },
+    { name: "Renault", icon: IconRenault },
+    { name: "Subaru", icon: IconSubaru },
+    { name: "Suzuki", icon: IconSuzuki },
+    { name: "Tesla", icon: IconTesla },
+    { name: "Toyota", icon: IconToyota },
+    { name: "Volkswagen", icon: IconVolkswagen },
+    { name: "Volvo", icon: IconVolvo },
+];
